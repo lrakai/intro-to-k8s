15352c27a161d6c88284986ca50dc3d14fc55ebf
lrakai <lmrakai@gmail.com>
2019-07-05 15:06:30 -0600

    Pin k8s version in ami version file

--- infra/config-environment.sh
+++ infra/config-environment.sh
@@ -1,3 +1,4 @@
 apt update
 apt-get install --only-upgrade kubeadm=1.15.0-00 kubectl=1.15.0-00 kubelet=1.15.0-00 kubernetes-cni=0.7.5-00 -y -f
+echo v1.15.0 > /etc/kubernetes_community_ami_version
 apt-get install ec2-instance-connect
