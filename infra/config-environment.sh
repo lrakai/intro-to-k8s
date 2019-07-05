deea0d261c60be1db057295d71a0895d9dccbfea
lrakai <lmrakai@gmail.com>
2019-07-05 14:52:04 -0600

    pre-upgrade to 1.15 before init cluster

--- infra/config-environment.sh
+++ infra/config-environment.sh
@@ -1,11 +1,3 @@
 apt update
 apt-get install --only-upgrade kubeadm=1.15.0-00 kubectl=1.15.0-00 kubelet=1.15.0-00 kubernetes-cni=0.7.5-00 -y -f
 apt-get install ec2-instance-connect
-
-# update master components
-if [ -e /etc/kubernetes/admin.conf ] ; then
-    # Can only upgrade 1 minor version at a time
-    snap install --channel 1.14/stable --classic kubeadm
-    /snap/kubeadm/current/kubeadm upgrade apply v1.14.3 -f
-    kubeadm upgrade apply v1.15.0 -f
-fi
